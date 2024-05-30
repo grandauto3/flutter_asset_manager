9496b05be8fe605f26b8592c636f627ce5770a19
Thilo S. <39314276+grandauto3@users.noreply.github.com>
2024-05-30 10:13:51 +0200

    add anyhow errors

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,21 @@
 mod ui;
 mod yaml;
 
-use std::io;
-
+use anyhow::{anyhow, Result};
 use crate::{
     ui::window,
     yaml::parser::{YamlParser},
 };
 
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     YamlParser::read_file()?;
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native("Window title",
                        native_options,
                        Box::new(|cc| Box::new(window::Ui::new(cc))),
-    ).unwrap();
+    ).map_err(|error: eframe::Error| anyhow!(error.to_string()))?;
 
     Ok(())
 }
